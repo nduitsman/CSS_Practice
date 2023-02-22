2f16ac20ab2449bbb427da6b827d21d5e2016da7
nduitsman <nduitsman@gmail.com>
2023-02-22 11:05:29 -0500

    added hacker style effect to .hacker class h1s

--- script.js
+++ script.js
@@ -8,3 +8,21 @@
 //         top: `${clientY}px`
 //     }, {duration: 200, fill: "forwards"});
 // }
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+document.querySelectorAll(".hacker").forEach(element => element.onmouseover = event => {
+    let iterations = 0;
+
+    const interval = setInterval(()=> {
+        event.target.innerText = event.target.innerText.split('')
+        .map((letter,index) => {
+            if(index < iterations) {
+                return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 26)]
+        }) 
+        .join('');
+        if(iterations >= event.target.dataset.value.length) clearInterval(interval);
+        iterations += 1/3;
+    }, 30);
+})
