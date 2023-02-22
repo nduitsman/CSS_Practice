50b4c1f06aa04e841be174e17ca11dddff00d991
nduitsman <nduitsman@gmail.com>
2023-02-21 19:24:46 -0500

    added blur and blob, with mouse trailing ability added (commented out)

---
+++ script.js
@@ -0,0 +1,10 @@
+// const blob = document.getElementById('blob');
+
+// document.body.onpointermove = event => {
+//     const { clientX, clientY } = event;
+
+//     blob.animate({
+//         left: `${clientX}px`,
+//         top: `${clientY}px`
+//     }, {duration: 200, fill: "forwards"});
+// }
